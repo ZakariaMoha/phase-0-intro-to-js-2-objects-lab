19c9f53277939d24a2f5382ca3ae203503d9339c
zakaria mohamed <zakmi.moha@gmail.com>
2023-09-24 17:25:04 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+function cloneObject(obj) {
+    return { ...obj };
+  }
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = cloneObject(employee);
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = cloneObject(employee);
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  const employee = {
+    name: 'Sam',
+  };
+  
+ 
+  
